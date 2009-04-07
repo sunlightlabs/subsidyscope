7cbddc66472bd4a0d6d5344611ac88bc426e99b2
tlee <tlee@8399c101-ab5c-441d-b403-44365f039031>
2009-04-07 18:20:12 +0000

    removed commend from bailout_pdfs; some of the requested changes from Lori on FDIC stuff
    git-svn-id: https://source.sunlightlabs.com/subsidyscope@585 8399c101-ab5c-441d-b403-44365f039031

--- bailout.site/trunk/bailout_pdfs/views.py
+++ bailout.site/trunk/bailout_pdfs/views.py
@@ -3,10 +3,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from bailout_pdfs.models import BailoutPDF
 import datetime
 
-# much of this configuration stuff should probably go live in local_settings.py
-# otoh, it's still just a one-off...
 
 def detail(request, pdf_id):
-    pdf = get_object_or_404(BailoutPDF, pk=pdf_id)
-    
+    pdf = get_object_or_404(BailoutPDF, pk=pdf_id)    
     return render_to_response('bailout_pdfs/bailoutpdf_detail.html', {'pdf': pdf, 'scribd_publisher_id': settings.SCRIBD_PUBLISHER_ID})
