abf7290ba299070fe3604599186ca9e4ae1eca1a
Thomas Lee <thomas.j.lee@gmail.com>
2009-04-10 10:32:07 -0400

    adding CSV view for H41 snapshots

--- bailout.site/trunk/fed_h41/urls.py
+++ bailout.site/trunk/fed_h41/urls.py
@@ -3,5 +3,6 @@ from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
     url(r'^\.xml$', 'fed_h41.views.h41_xml', name="h41_snapshot_xml"),      
+    url(r'^\.csv$', 'fed_h41.views.h41_csv', name="h41_snapshot_csv"),
     url(r'^\/news\.xml$', 'fed_h41.views.fed_news_xml', name="fed_news_xml"),        
 )
--- bailout.site/trunk/fed_h41/views.py
+++ bailout.site/trunk/fed_h41/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse
 import csv
 import datetime
+import string
 
 def h41_xml(request):    
     
@@ -36,4 +37,62 @@ def fed_news_xml(request):
     news = FedNewsEvent.objects.all().order_by('date')
     return render_to_response('bailout/federal_reserve/news.xml', { 'news': news }, mimetype='text/xml')
     
-        
+
+def h41_csv(request):
+    # Create the HttpResponse object with the appropriate CSV header.
+    response = HttpResponse(mimetype='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=subsidyscope-fed-h41-snapshots.csv'
+
+    writer = csv.writer(response)
+
+    field_name_substitutions = {       
+    }    
+    columns_to_exclude = ['id']
+    currency_columns = [
+        'reserve_bank_credit',
+        'repurchase_agreements',
+        'primary_credit',
+        'secondary_credit',
+        'seasonal_credit',
+        'other_credit_extensions',
+        'mortgage_backed_securities',
+        'term_auction_credit',
+        'primary_dealer_and_other_broker_dealer_credit',
+        'asset_backed_commercial_paper_money_market',
+        'credit_extended_to_aig',
+        'commercial_paper_funding_facility',
+        'money_market_investor_funding_facility',
+        'maiden_lane_i',
+        'maiden_lane_ii',
+        'maiden_lane_iii',
+        'federal_agency_debt_securities',
+        'term_facility',
+        'talf',
+        'other_federal_reserve_assets',
+        'central_bank_liquidity_swaps',
+        'us_treasury_securities'
+    ]
+
+    headers = []
+    for field in H41Snapshot._meta.fields:
+        if field.name not in columns_to_exclude:
+            if field.name in field_name_substitutions:
+                headers.append(field_name_substitutions[field.name])
+            else:
+                headers.append(field.verbose_name)
+    writer.writerow(headers)
+
+    snapshots = H41Snapshot.objects.all().order_by('date')
+    for snapshot in snapshots:
+
+        record = []
+        for field in H41Snapshot._meta.fields:
+            if field.name not in columns_to_exclude:
+                value = getattr(snapshot, field.name, None)
+                if field.name in currency_columns and value is not None:
+                    value = int(value) * 1000000                
+                record.append(value)
+
+        writer.writerow(record)
+
+    return response
