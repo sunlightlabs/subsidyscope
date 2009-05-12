0fa84c4701553d46ed8a155fcedaff8129518bc2
David James <djames@sunlightfoundation.com>
2009-05-12 11:59:46 -0400

    Add index to slug

--- bailout.site/trunk/glossary/models.py
+++ bailout.site/trunk/glossary/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib import admin
 
 class Item(models.Model):
-    slug       = models.SlugField()
+    slug       = models.SlugField(db_index=True)
     term       = models.CharField(max_length=255, db_index=True)
     definition = models.TextField()
 
