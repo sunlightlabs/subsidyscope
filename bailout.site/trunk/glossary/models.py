941899e7e44dfe71915c035d0b202bc317c279eb
David James <djames@sunlightfoundation.com>
2009-05-13 18:02:35 -0400

    Updating glossary Item model
    Now includes
     * a 'before save' hook
     * a column for the term length (will be used by
       the glossarize method in the future)

--- bailout.site/trunk/glossary/models.py
+++ bailout.site/trunk/glossary/models.py
@@ -1,9 +1,27 @@
 from django.db import models
 from django.contrib import admin
+from django.template.defaultfilters import slugify
+
 
 class Item(models.Model):
-    slug       = models.SlugField(db_index=True, unique=True)
-    term       = models.CharField(max_length=255, unique=True, db_index=True)
-    definition = models.TextField()
+    slug        = models.SlugField(db_index=True, unique=True)
+    term        = models.CharField(max_length=255, unique=True, db_index=True)
+    term_length = models.IntegerField(null=True, db_index=True)
+    definition  = models.TextField()
+
+    def __unicode__(self):
+        return u"%s" % self.term
+
+    def save(self):
+        self.update_term_length()
+        self.autogenerate_slug_if_blank()
+        super(Item, self).save()
+
+    def update_term_length(self):
+        self.term_length = len(self.term) if self.term else 0
+
+    def autogenerate_slug_if_blank(self):
+        if self.term and not self.slug:
+            self.slug = slugify(self.term)
 
 admin.site.register(Item)
