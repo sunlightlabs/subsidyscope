207f7d050685c26301ee62ae061eb26e82a85358
David James <djames@sunlightfoundation.com>
2009-05-13 14:11:55 -0400

    glossary: slug, term must be unique

--- bailout.site/trunk/glossary/models.py
+++ bailout.site/trunk/glossary/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib import admin
 
 class Item(models.Model):
-    slug       = models.SlugField(db_index=True)
-    term       = models.CharField(max_length=255, db_index=True)
+    slug       = models.SlugField(db_index=True, unique=True)
+    term       = models.CharField(max_length=255, unique=True, db_index=True)
     definition = models.TextField()
 
 admin.site.register(Item)
