f6a3389cfd2b4495eec2300b5ca1d10b867205fe
David James <djames@sunlightfoundation.com>
2009-05-16 18:43:07 -0400

    formatting for PEP8

--- bailout.site/trunk/glossary/helpers.py
+++ bailout.site/trunk/glossary/helpers.py
@@ -2,6 +2,7 @@ from models import Item
 from django.core.urlresolvers import reverse
 from utils.multiple_sub import multiple_sub
 
+
 def glossarize(plain):
     """
     Converts a string into a hyperlinked string.
@@ -11,7 +12,9 @@ def glossarize(plain):
     """
     base_url = reverse("glossary")
     items = Item.objects.order_by('-term_length')
+
     def link(item):
         return """<a href="%s#%s">%s</a>""" % (base_url, item.slug, item.term)
+
     mapping = [(item.term, link(item)) for item in items]
     return multiple_sub(plain, mapping)
