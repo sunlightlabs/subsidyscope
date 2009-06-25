f8905821ea8557d8c8eaa4a0557953bc2e94a559
U-KPW\Kevin Webb <Kevin Webb@KPW.(none)>
2009-06-25 15:50:32 -0400

    updated eas route subsidy calc

--- bailout.site/trunk/eas/models.py
+++ bailout.site/trunk/eas/models.py
@@ -77,23 +77,60 @@ class EASRoute(models.Model):
         route_list = []
         
         for hub in self.hub_airport.all():
-            route_list.append(RouteStatistics.objects.filter(origin_airport=self.eas_airport, destination_airport=hub))
-            route_list.append(RouteStatistics.objects.filter(destination_airport=self.eas_airport, origin_airport=hub))
+            route_list.append(RouteStatistics.objects.filter(carrier=self.carrier, origin_airport=self.eas_airport, destination_airport=hub))
+            route_list.append(RouteStatistics.objects.filter(carrier=self.carrier, destination_airport=self.eas_airport, origin_airport=hub))
         
         routes = set()
-                
+        
+        start_year = self.eas_start_date.year
+        start_month = self.eas_start_date.month
+        
+        end_year = self.eas_end_date.year
+        end_month = self.eas_end_date.month    
+        
         for route in route_list:
             
             routes_tmp = routes
             data_tmp = set()
             
             for data in route:
-                data_tmp.add(data)
+                if (data.year > start_year or (data.year == start_year and data.month >= start_month)) and (data.year < end_year or (data.year == end_year and data.month <= end_month)):
+                    data_tmp.add(data)
             
             routes = routes_tmp.union(data_tmp)
-                
+              
+        total_flights = 0
+        total_time = 0
+        total_passengers =0
         
-        print len(routes)
+        try:
+            distances = []
+            
+            for route in routes:
+                
+                total_flights += route.departures_performed
+                total_time += route.ramp_time
+                total_passengers += route.passengers
+                
+                distances.append(route.distance)
+            
+            min_distance = min(distances)    
+            max_distance = max(distances)
+            
+            total_subsidy = self.eas_flight_subsidy * total_flights
+            
+            passenger_subsidy = float(total_subsidy) / float(total_passengers)
+            
+            passenger_mile_subsidy = passenger_subsidy / float(max_distance)
+            
+            print self
+            print '\tFlights: %d' % total_flights
+            print '\tTotal Subsidy: $%d' % total_subsidy
+            print '\tPassenger Subsidy: %f' % passenger_subsidy
+            print '\tPassenger Mile Subsidy: %f' % passenger_mile_subsidy
+        except:
+            pass
+        
         
     
 class RouteStatistics(models.Model):
