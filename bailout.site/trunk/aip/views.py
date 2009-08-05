c573931b6ce17ce7ed184ce7f72045fadf11d7fd
Kaitlin <kaitlin@system76-pc.(none)>
2009-08-05 14:14:34 -0400

    more aip updates (pt II)

--- bailout.site/trunk/aip/views.py
+++ bailout.site/trunk/aip/views.py
@@ -7,20 +7,25 @@ def index(request):
     ports = None
     grants = None
     error = None
+    type = None
     if request.method == 'GET' and request.GET:
         if request.GET.__contains__('portname') and request.GET['portname'] != '':
             ports = Airport.objects.filter(name__icontains=(request.GET['portname'].strip(' ')))
-            if ports and len(ports) >= 1:
-                grants = []
-                for p in ports:
-                    pgrants = GrantRecord.objects.filter(airport=p)
-                    money = 0
-                    for m in pgrants:
-                        money += m.amount
-                    grants.append((p, money))
-                return render_to_response('aip/index.html', {'ports':ports, 'grants': grants})
-            else:
-                error = "No airport matched the name provided"
+            type="name"
+        elif request.GET.__contains__('state'):
+            ports = Airport.objects.filter(state__iexact=request.GET['state'])
+            type = "state"
+        if ports and len(ports) >= 1:
+            grants = []
+            total = 0
+            for p in ports:
+                pgrants = GrantRecord.objects.filter(airport=p)
+                money = 0
+                for m in pgrants:
+                    money += m.amount
+                total += money
+                grants.append((p, money))
+            return render_to_response('aip/index.html', {'ports':ports, 'grants': grants, 'total': total, 'type': type})
         elif request.GET.__contains__('portcode'):
             ports = Airport.objects.get(code__iexact=request.GET['portcode'])
             if ports:
@@ -33,9 +38,17 @@ def index(request):
                 return render_to_response('aip/index.html', {'port':ports, 'grants': grants, 'params':[request.GET['portname'], request.GET['portcode']]})
             else:
                 error = "No airport matched the code you specified"
+
         else:
             error = "Must specify and airport name or airport code"
         if error:
             return render_to_response('aip/index.html', {'error': error, 'params': [request.GET['portname'], request.GET['portcode']]})
     else: 
         return render_to_response('aip/index.html', {'error': 'not a get'})
+
+def portdetail(request):
+    if request.GET.__contains__('code'):
+        port = Airport.objects.get(code__iexact=request.GET['code'])
+        if port:
+            portgrants = GrantRecord.objects.filter(airport=port)    
+        return render_to_response('aip/detail.html', {"grants":portgrants})
