ed025331128a71b9a2c63fc105a69ea28cc4232e
Thomas Lee <thomas.j.lee@gmail.com>
2009-08-07 13:57:23 -0400

    changing 'direct payments' to 'direct expenditures'

--- bailout.site/trunk/transportation/urls.py
+++ bailout.site/trunk/transportation/urls.py
@@ -7,11 +7,11 @@ from django.views.generic.simple import direct_to_template, redirect_to
 
 
 urlpatterns = patterns('', 
-    url(r'^direct-payments/search/', 'faads.views.search', {'sector_name': 'transportation'}, name='transportation-faads-search'),
-    url(r'^direct-payments/programs/faads/(?P<cfda_id>[0-9]+)/', 'cfda.views.getFAADSLineItems',{'sector_name':'transportation'}, name='cfda-FAADS'),
-    url(r'^direct-payments/programs/(?P<cfda_id>[0-9]+)/', 'cfda.views.getProgram', {'sector_name': 'transportation'}, name='cfda-programpage'),
-    url(r'^direct-payments/programs/$', 'cfda.views.getProgramIndex', {'sector_name': 'transportation'}, name='cfdaprograms-index'),
-    url(r'^direct-payments/', direct_to_template, {'template': 'transportation/direct_payments.html'}, name='transportation-direct-payments'),
+    url(r'^direct-expenditures/search/', 'faads.views.search', {'sector_name': 'transportation'}, name='transportation-faads-search'),
+    url(r'^direct-expenditures/programs/faads/(?P<cfda_id>[0-9]+)/', 'cfda.views.getFAADSLineItems',{'sector_name':'transportation'}, name='cfda-FAADS'),
+    url(r'^direct-expenditures/programs/(?P<cfda_id>[0-9]+)/', 'cfda.views.getProgram', {'sector_name': 'transportation'}, name='cfda-programpage'),
+    url(r'^direct-expenditures/programs/$', 'cfda.views.getProgramIndex', {'sector_name': 'transportation'}, name='cfdaprograms-index'),
+    url(r'^direct-expenditures/', direct_to_template, {'template': 'transportation/direct_payments.html'}, name='transportation-direct-expenditures'),
     url(r'^tax-expenditures/', direct_to_template, {'template': 'transportation/tax_expenditures.html'}, name='transportation-tax-expenditures'),
     url(r'^contracts/', direct_to_template, {'template': 'transportation/contracts.html'}, name='transportation-contracts'),
     url(r'^backgrounders/', direct_to_template, {'template': 'transportation/backgrounders.html'}, name='transportation-backgrounders'),
