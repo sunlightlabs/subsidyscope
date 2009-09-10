d57ca5b7da74632a0dd6889e82e958ac3487901d
Thomas Lee <thomas.j.lee@gmail.com>
2009-09-10 16:11:34 -0400

    IE doesn't support the change() event properly; moving to use click() instead

--- bailout.site/trunk/media/scripts/faads-search.js
+++ bailout.site/trunk/media/scripts/faads-search.js
@@ -11,23 +11,13 @@ function DisplayProgramPanel(panel)
 
 }
 
-$(function(){
-    
-    // wrap checkbox label contents
-    $('#program-selection-program ul li label').each(function(i){
-        var parts = $(this).html().split('>');
-        var checkbox = parts.shift();
-        var rest = parts.join('>');
-       $(this).html(checkbox + '> <span class="program-title">' + rest.replace(/^\s+/,'') + '</span>'); 
-    });
-    
+$(function(){    
     
     // set up "select all"/"select none" links
     $('.select-all-or-none a.select-all').click(function(){
        $(this).parent().parent().find('ul li input[type=checkbox]').attr('checked', true); 
        return false;
-    });
-    
+    });    
     $('.select-all-or-none a.select-none').click(function(){
        $(this).parent().parent().find('ul li input[type=checkbox]').attr('checked', false); 
        return false;
@@ -37,38 +27,11 @@ $(function(){
     $('fieldset legend').click(function(){
        $(this).parent().toggleClass('collapsed'); 
     });
-    
-    // $('select[multiple]').multiSelect(  
-    // {  
-    //     select_all_min: 3,  
-    //     no_selection: "No criteria selected",  
-    //     selected_text: " selected"  
-    // });
-
-    // hook up tags dropdown to CFDA
-    $('#id_tags').change(function(){
-      var cfda_program_numbers = $(this).val().split(',');
-      var label = $(this).get(0).options[$(this).get(0).selectedIndex].text;
-      $('#cfda-programs input[type=checkbox]').attr('checked', false);
-      for(var i=0; i<cfda_program_numbers.length; i++)
-      {
-        var program_number = cfda_program_numbers[i];
-        $('#cfda-programs input[value=' + program_number + ']').attr('checked',true);
-      }
-      $(this).val('');
-      $('#multiSelect-id_cfda_programs-title').text('All ' + label + ' Programs');
-    });
-    
-    // display active program selection dialog
-    $('#program-selection-' + $('#program-selector ul li input:checked').val()).slideDown();
-    
+        
     // hook up program radio buttons
-    $('#program-selector ul li input').change(function(){
+    $('#program-selector ul li label input').click(function(){
         DisplayProgramPanel($('#program-selector ul li input:checked').val());
-    });
-    
-    // rename multiselect title -- necessary?
-    $('#multiSelect-id_cfda_programs-title').text('Select specific CFDA programs')
+    });    
 
     // hook up datepicker
     $('#obligation-date-wrapper input[type=text]').datepicker();
