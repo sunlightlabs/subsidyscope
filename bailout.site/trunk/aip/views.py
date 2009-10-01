2f90da7f012d9848334e2d7dd6bc22efa2a3ff57
kaitlin <kaitlin@kaitlin-desktop.(none)>
2009-10-01 15:17:25 -0400

    aip interface overhaul part 1

--- bailout.site/trunk/aip/views.py
+++ bailout.site/trunk/aip/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
 from django.shortcuts import render_to_response, get_object_or_404
 from aip.models import *
 from django.db.models import Avg, Sum
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
 import logging
 import logging.handlers
 
@@ -26,27 +27,75 @@ def portdata (request, code):
         totalfunding = grants.aggregate(Sum('total'))
         return render_to_response('aip/airports.html', {'airport': airport,'enplanements': enplanements, 'operations':operations, 'grants':grants, 'projects':projects, 'stimulus': stimulus, 'totalfunding':totalfunding['total__sum'], 'avggrant':avggrant['total__avg'], 'avgops':avgops['operations__avg'], 'avgenps':avgenps['amount__avg']})
 
+def get_districts():
+    from django.db import connection, transaction
+    cursor=connection.cursor()
+    cursor.execute("select distinct state, district from aip_airport order by state")
+    rows = cursor.fetchall()
+    districts = []
+    for r in rows:
+        if r[1]:
+            districts.append("%s-%s" % (r[0], r[1]))
+    return districts
+
+def get_years():
+    from django.db import connection, transaction
+    cursor=connection.cursor()
+    cursor.execute("select distinct fiscal_year from aip_grantrecord")
+    rows = cursor.fetchall()
+    years = []
+    for r in rows:
+        years.append(r[0])
+    return years
 
 def index(request):
-    ports = None
+    ports = []
     grants = None
     error = None
-    type = None
-    #blockgs = 0
-    if request.method == 'GET' and request.GET:
-        if request.GET.__contains__('portname') and request.GET['portname'] != '':
-            ports = Airport.objects.filter(name__icontains=(request.GET['portname'].strip(' ')))
-            type="name"
-        elif request.GET.__contains__('state'):
-            stateabbrev = request.GET['state']
+    parameter = None
+    get = request.GET
+    subtype = None
+    districts = get_districts()
+    years = get_years()
+    nprs = []
+    for n in range(101):
+        nprs.append(n)
+    if get.__contains__('searchtype'):
+        type = get['searchtype']
+    else: 
+        return render_to_response('aip/index.html', {'districts':districts, 'years':years, 'nprs':nprs})
+
+    if type=='airport':
+        if get.__contains__('portcode') and get['portcode'] != '':
+            portcode = get['portcode']
+            try:
+                ports.append(Airport.objects.get(code__iexact=portcode))
+                subtype = 'code'
+            except Airport.DoesNotExist:
+                error = 'No airport matched the parameters you specified'
+
+        elif get.__contains__('portname') and get['portname'] != '':
+            subtype = 'name'
+            portname = get['portname']
+            parameter = portname
+            ports = Airport.objects.filter(name__icontains=(portname.strip(' ')))
+
+        elif get.__contains__('state') and get['state'] != '':
+            subtype='state'
+            stateabbrev = get['state']
+            parameter = stateabbrev
             if stateabbrev == 'FSM':
                 ports = Airport.objects.filter(state__in=['FM', 'MH', 'PW'])
             else:
                 ports = Airport.objects.filter(state__iexact=stateabbrev)
-            type = "state"
-     #       bgrants = BlockGrant.objects.filter(state__iexact=request.GET['state'])
-      #      for b in bgrants:
-       #         blockgs += b.amount
+        elif get.__contains__('district') and get['district'] != '':
+            dist = get['district']
+            subtype="district"
+            parameter = dist
+            state = dist.split('-')[0]
+            dnum = dist.split('-')[1]
+            ports = Airport.objects.filter(state__iexact=state, district=dnum)
+
         if ports and len(ports) >= 1:
             grants = []
             total = 0
@@ -62,30 +111,60 @@ def index(request):
                     money += m.total
                 total += money
                 grants.append((p, money, enplanements))
-      #      total += blockgs
-            return render_to_response('aip/index.html', {'ports':ports, 'grants': grants, 'total': total, 'type': type})
-        elif request.GET.__contains__('portcode'):
-            try:
-                ports = Airport.objects.get(code__iexact=request.GET['portcode'])
-                if ports:
-                    grants = []
-                    pgrants = GrantRecord.objects.filter(airport=ports)
-                    enps = Enplanements.objects.filter(airport=ports)
-                    money = 0
-                    enplanements = 0
-                    for m in pgrants:
-                        money += m.total
-                    for e in enps:
-                        enplanements += e.amount
-                    grants.append((ports, money, enplanements))
-                    return render_to_response('aip/index.html', {'port':ports, 'grants': grants, 'params':[request.GET['portname'], request.GET['portcode']]})
-            except Airport.DoesNotExist:
-                error = 'No airport matched the parameters you specified'
+            
+            return render_to_response('aip/index.html', {subtype: parameter, 'ports':ports, 'grants': grants, 'total': total, 'type': type, 'districts': districts, 'years':years, 'nprs':nprs})
 
-        else:
+    elif type=="project":
+        projects = Project.objects.all()
+        year_first = get['year_range_first']
+        year_last = get['year_range_last']
+        npr_first = get['npr_range_first']
+        npr_last = get['npr_range_last']
+        dist_filter = get['district_filter']
+        
+        if get.__contains__('stimulus'):
+            stimulus = True
+        else: stimulus = False
+        
+        if dist_filter != '':
+            state = dist_filter.split('-')[0]
+            dnum = dist_filter.split('-')[1]
+            portset = Airport.objects.filter(state__iexact=state, district=dnum)
+            projects = Project.objects.filter(airport__in=portset)
+
+        if year_first != 2005 or year_last != 2009:
+            projects = projects.filter(fiscal_year__gte=year_first, fiscal_year__lte=year_last)
+        if npr_first !=0 or npr_last != 100:
+            projects = projects.filter(npr__gte=npr_first, npr__lte=npr_last)
+        if stimulus:
+            projects = projects.filter(stimulus__gt=0)
+
+        if get['fysort'] == 'asc': projects = projects.order_by('fiscal_year')
+        elif get['fysort'] == 'desc': projects = projects.order_by('-fiscal_year')
+        if get['nprsort'] == 'asc': projects = projects.order_by('npr')
+        elif get['nprsort'] == 'desc': projects = projects.order_by('-npr')
+        if get['airportsort'] == 'asc': projects = projects.order_by('airport')
+        elif get['airportsort'] == 'desc': projects = projects.order_by('-airport')
+        if get['totalsort'] == 'asc': projects = projects.order_by('total')
+        elif get['totalsort'] == 'desc': projects = projects.order_by('-total')
+        
+        sorts = {'fysort': get['fysort'], 'nprsort':get['nprsort'], 'airportsort':get['airportsort'], 'totalsort':get['totalsort']}
+
+        paginator = Paginator(projects, 25)
+        try:
+            page = int(get.get('page', '1'))
+        except ValueError:
+            page = 1
+
+        try:
+            projects = paginator.page(page)
+        except (EmptyPage, InvalidPage):
+            projects = paginator.page(paginator.num_pages)
+
+        return render_to_response('aip/index.html', {subtype: parameter, 'projects':projects, 'districts': districts, 'years':years, 'nprs':nprs, 'querystring':request.META['QUERY_STRING'], 'sorts':sorts})
+        
+    else:
+        if not error:
             error = 'You must specify and airport name or airport code'
-        if error:
-            return render_to_response('aip/index.html', {'error': error})
-    else: 
-        return render_to_response('aip/index.html')
+        return render_to_response('aip/index.html', {'error': error})
 
