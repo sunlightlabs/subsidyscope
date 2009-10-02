b6b80d2f405a8cb636bc9003fcdf198242aac8d9
Thomas Lee <thomas.j.lee@gmail.com>
2009-10-02 14:03:38 -0400

    adding page for Deposit Insurance Fund update page

--- bailout.site/trunk/fdic_bank_failures/urls.py
+++ bailout.site/trunk/fdic_bank_failures/urls.py
@@ -4,6 +4,7 @@ from django.views.generic.simple import direct_to_template, redirect_to
 urlpatterns = patterns('',
     url(r'^$', 'fdic_bank_failures.views.fdic_bank_failures', name='fdic_bank_failures'),
     url(r'^table\/$', 'fdic_bank_failures.views.fdic_bank_failures_table', name='fdic_bank_failures_table'),
+    url(r'^prepayment-requirement\/$', direct_to_template, {'template': 'bailout/bailout.html'}, name='fdic_bank_failures_prepayment_requirement'),
     url(r'^bank-failures\.xml$', 'fdic_bank_failures.views.fdic_bank_failures_xml', name="fdic_bank_failures_xml"),
     url(r'^bank-failures\.csv$', redirect_to, {'url':'/media/data/bailout/fdic_bank_failures.csv'}, name="fdic_bank_failures_csv"),    
     url(r'^qbp-snapshots\.xml$', 'fdic_bank_failures.views.fdic_qbpsnapshot_xml', name="fdic_qbpsnapshot_xml"),        
