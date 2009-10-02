c56f3d88641ecd842b1570163cd563469b5cbf7f
kaitlin <kaitlin@kaitlin-desktop.(none)>
2009-10-02 18:07:42 -0400

    added clause for special chars in airport code

--- bailout.site/trunk/aip/urls.py
+++ bailout.site/trunk/aip/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template, redirect_to
 
 urlpatterns = patterns('',
-    url(r'^airports/(?P<code>[A-Za-z0-9*+]+)', 'aip.views.portdata', name='aip-airport-data'),
+    url(r'^airports/(?P<code>[A-Za-z0-9@*+]+)', 'aip.views.portdata', name='aip-airport-data'),
     url(r'^$', 'aip.views.index', name='aip-index')
 )
