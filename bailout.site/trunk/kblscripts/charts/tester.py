9b6f562ad60c9e2e76d1842af803ffc2ce00166b
kaitlin <kaitlin@kaitlin-desktop.(none)>
2010-03-15 10:10:45 -0400

    line graph changes

--- bailout.site/trunk/kblscripts/charts/tester.py
+++ bailout.site/trunk/kblscripts/charts/tester.py
@@ -1,7 +1,9 @@
 
 from charts import *
 
-g = Column(300, 400, [[('aaaaaaaaaa',3), ('bbbbbbbbbb',4)], [('aaaaaaaaaa',3), ('bbbbbbbbbb',2), ('cccc', 5)]], 'Column', "charts.css")
+g = Column(300, 400, [[('aaaaaaaaaa',500), ('bbbbbbbbbb',800)], [('aaaaaaaaaa',230), ('bbbbbbbbbb',260), ('cccc', 300)]], 'Column', "barchart.css")
 
-g.output()
+g.output("bar.svg")
 
+h = Line(300, 400, [[(1,2000), (3,2001), (4,2002), (5, 2003)], [(3, 2000),  (5, 2002), (3, 2003)]], 'Line', 'linechart.css', label_intervals=2)
+h.output("line.svg")
