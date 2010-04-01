86f51017a4ad5046c22458461bacee352523720d
kaitlin <kaitlin@kaitlin-desktop.(none)>
2010-04-01 11:08:25 -0400

    changed dataset around to test

--- bailout.site/trunk/kblscripts/charts/tester.py
+++ bailout.site/trunk/kblscripts/charts/tester.py
@@ -5,5 +5,5 @@ g = Column(300, 400, [[('aaaaaaaaaa',500), ('bbbbbbbbbb',800)], [('aaaaaaaaaa',2
 
 g.output("bar.svg")
 
-h = Line(300, 400, [[(1,2000), (3,2001), (4,2002), (5, 2003)], [(3, 2000),  (5, 2002), (3, 2003)]], 'Line', 'linechart.css', label_intervals=2)
+h = Line(300, 400, [[(2000, 10), (2001, 30), (2002, 40), (2003, 50)], [(2000, 30),  (2002, 40), (2003, 50)]], 'Line', 'linechart.css', label_intervals=2)
 h.output("line.svg")
