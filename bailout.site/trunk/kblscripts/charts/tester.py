dd8ccc08fa747ad41bbd39722e1fb83b9b6210e5
kaitlin <kaitlin@kaitlin-desktop.(none)>
2010-04-29 18:27:59 -0400

    just made this better in general

--- bailout.site/trunk/kblscripts/charts/tester.py
+++ bailout.site/trunk/kblscripts/charts/tester.py
@@ -11,3 +11,12 @@ h.output("line.svg")
 h = PieChart(400, 400, [[(2000, 10), (2001, 30), (2002, 40), (2003, 50)]], 'Pie', 'piechart.css')
 h.output("pie.svg")
 
+
+#risk transfers
+risk_transfers = Column(185, 220, [[('Outstanding Credit', 7445983679), ('Subsidy', 114141251)]], 'Column', "barchart.css", padding=10)
+risk_transfers.output("risk_transfers.svg")
+
+#contracts
+
+contracts = Line(230, 315, [[(2000, 12972951342), (2001, 14109441817), (2002, 15780150198), (2003, 20911608531), (2004, 21064466760), (2005, 15275901189), (2006, 15939230914), (2007, 19124684255), (2008, 18239197959), (2009, 16226290107)]], 'Line', 'linechart.css', label_intervals=2, units='', currency=False)
+contracts.output("contracts.svg")
