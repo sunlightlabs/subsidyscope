c0b327a756e2db93b883c670961aa96f18a102a7
Tom Lee <tlee@sunlightfoundation.com>
2010-06-07 17:47:21 -0400

    revising helper shell scripts to account for cleaned-up fpds/faads management command names

--- bailout.site/trunk/do_faads_import.sh
+++ bailout.site/trunk/do_faads_import.sh
@@ -15,7 +15,7 @@ while [ "$var0" -lt "$LIMIT" ]
 # Spaces, because these are "test-brackets" . . .
 do
   echo "running loop $var0..."
-  ./manage.py do_faads_import
+  ./manage.py faads_import
   var0=`expr $var0 + 1`
 done
 
