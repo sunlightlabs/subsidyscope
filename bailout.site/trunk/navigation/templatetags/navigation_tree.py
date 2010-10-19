c1c4abfbf3089c622ee438ad2a5150d02eb286ce
Kevin <kwebb@sunlightfoundation.com>
2010-10-19 18:29:38 -0400

    nav tree for tes

--- bailout.site/trunk/navigation/templatetags/navigation_tree.py
+++ bailout.site/trunk/navigation/templatetags/navigation_tree.py
@@ -378,6 +378,27 @@ SECTORS = { 'sector': 'home',
                                           'children':[]
                                         }
                                     ]
+                        },
+                        
+                        { 'sector': 'tax_expenditures',
+                           'name': 'Tax Expenditures',
+                           'url_name': 'tax_expenditures-summary',
+                           'children': [{'sector': 'tax_expenditures',
+                                          'name': 'Summary',
+                                          'url_name': 'tax_expenditures-summary',
+                                          'children':[]
+                                        },
+                                        {'sector': 'tax_expenditures',
+                                          'name': 'Methodology',
+                                          'url_name': 'tax_expenditures-methodology',
+                                          'children':[]
+                                        },
+                                        {'sector': 'tax_expenditures',
+                                          'name': 'Database',
+                                          'url_name': 'tax_expenditures-main',
+                                          'children':[]
+                                        }
+                                    ]
                         }
                 ]
             }
