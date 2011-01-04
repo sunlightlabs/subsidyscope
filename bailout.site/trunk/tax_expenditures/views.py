28cd0a6633254b0926f74d0e939e97b4b55cfbc1
Kevin <kwebb@sunlightfoundation.com>
2011-01-04 13:40:23 -0500

    pted csv updates

--- bailout.site/trunk/tax_expenditures/views.py
+++ bailout.site/trunk/tax_expenditures/views.py
@@ -94,7 +94,7 @@ def te_csv(request, group_id=None):
     writer = csv.writer(response)
     
     
-    writer.writerow(['Indent', 'Budget Function', 'Category', 'Title as Appears in Budget', 'Source','Report', 'Footnotes', '2000 Total','2001 Total','2002 Total','2003 Total','2004 Total','2005 Total','2006 Total','2007 Total','2008 Total','2009 Total','2010 Total','2011 Total','2012 Total','2013 Total','2014 Total','2015 Total','2000 Corp','2001 Corp','2002 Corp','2003 Corp','2004 Corp','2005 Corp','2006 Corp','2007 Corp','2008 Corp','2009 Corp','2010 Corp','2011 Corp','2012 Corp','2013 Corp','2014 Corp','2015 Corp','2000 Indv','2001 Indv','2002 Indv','2003 Indv','2004 Indv','2005 Indv','2006 Indv','2007 Indv','2008 Indv','2009 Indv','2010 Indv','2011 Indv','2012 Indv','2013 Indv','2014 Indv','2015 Indv'])
+    writer.writerow(['Indent', 'Budget Function', 'Subsidyscope Title', 'Title as Appears in Budget', 'Source','Report', 'Footnotes', '2000 Total','2001 Total','2002 Total','2003 Total','2004 Total','2005 Total','2006 Total','2007 Total','2008 Total','2009 Total','2010 Total','2011 Total','2012 Total','2013 Total','2014 Total','2015 Total','2000 Corp','2001 Corp','2002 Corp','2003 Corp','2004 Corp','2005 Corp','2006 Corp','2007 Corp','2008 Corp','2009 Corp','2010 Corp','2011 Corp','2012 Corp','2013 Corp','2014 Corp','2015 Corp','2000 Indv','2001 Indv','2002 Indv','2003 Indv','2004 Indv','2005 Indv','2006 Indv','2007 Indv','2008 Indv','2009 Indv','2010 Indv','2011 Indv','2012 Indv','2013 Indv','2014 Indv','2015 Indv'])
     
     if parent:
         
@@ -138,27 +138,42 @@ def recurse_category(parent, writer, indent, budget_function):
         
         for estimate in expenditure.estimate_set.all():
             
-            total_estimates[estimate.estimate_year] = 0
+            total_estimates[estimate.estimate_year] = None
             
             if estimate.corporations_notes == Estimate.NOTE_POSITIVE:
+                total_estimates[estimate.estimate_year] = 0
                 corp_estimates[estimate.estimate_year] = '<50'
             elif estimate.corporations_notes == Estimate.NOTE_NEGATIVE:
+                total_estimates[estimate.estimate_year] = 0
                 corp_estimates[estimate.estimate_year] = '>-50'
             else:
                 corp_estimates[estimate.estimate_year] = estimate.corporations_amount
                 
                 if estimate.corporations_amount:
-                    total_estimates[estimate.estimate_year] += estimate.corporations_amount
+                    total_estimates[estimate.estimate_year] = estimate.corporations_amount
             
             if estimate.individuals_notes == Estimate.NOTE_POSITIVE:
+                
+                if not total_estimates[estimate.estimate_year]:
+                    total_estimates[estimate.estimate_year] = 0
+                    
                 indv_estimates[estimate.estimate_year] = '<50'
+                
             elif estimate.individuals_notes == Estimate.NOTE_NEGATIVE:
+                
+                if not total_estimates[estimate.estimate_year]:
+                    total_estimates[estimate.estimate_year] = 0
+                
                 indv_estimates[estimate.estimate_year] = '>-50'
+                
             else:
                 indv_estimates[estimate.estimate_year] = estimate.individuals_amount
                 
                 if estimate.individuals_amount:
-                    total_estimates[estimate.estimate_year] += estimate.individuals_amount
+                    if total_estimates[estimate.estimate_year]:
+                        total_estimates[estimate.estimate_year] += estimate.individuals_amount
+                    else:
+                        total_estimates[estimate.estimate_year] = estimate.individuals_amount
         
         
         for year in TE_YEARS:
