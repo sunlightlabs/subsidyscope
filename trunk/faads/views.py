f605feabaeb324c94122049de5af5e290aaa7452
Kevin Webb <kwebb@sunlightfoundation.com>
2011-02-10 14:05:46 -0500

    updated faads csv code

--- trunk/faads/views.py
+++ trunk/faads/views.py
@@ -34,9 +34,10 @@ RESULTS_PER_PAGE = getattr(settings, 'HAYSTACK_FAADS_SEARCH_RESULTS_PER_PAGE', g
 def strip_clean_tags(x):
     
     if x:
-        return strip_tags(x)
+        return strip_tags(x.encode('ascii','ignore'))
     else:
         return ''
+    
 
 def MakeFAADSSearchFormClass(sector=None, subsectors=[]):
     
