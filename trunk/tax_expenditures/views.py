f102660096c1d5005167cbc5b90e9768f8042ad4
kaitlin <kaitlin@kaitlin-work.(none)>
2011-03-02 14:09:19 -0500

    changed csv code

--- trunk/tax_expenditures/views.py
+++ trunk/tax_expenditures/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from tax_expenditures.models import Group, GroupSummary, Expenditure, Estimate, TE_YEARS
 
-
+SOURCES = ('', 'JCT', 'Treasury')
 MAX_COLUMNS = 11
 
 
@@ -93,9 +93,9 @@ def te_csv(request, group_id=None):
 
     writer = csv.writer(response)
     
-    
-    writer.writerow(['Indent', 'Budget Function', 'Subsidyscope Title', 'Title as Appears in Budget', 'Source','Report', 'Footnotes', '2000 Total','2001 Total','2002 Total','2003 Total','2004 Total','2005 Total','2006 Total','2007 Total','2008 Total','2009 Total','2010 Total','2011 Total','2012 Total','2013 Total','2014 Total','2015 Total','2000 Corp','2001 Corp','2002 Corp','2003 Corp','2004 Corp','2005 Corp','2006 Corp','2007 Corp','2008 Corp','2009 Corp','2010 Corp','2011 Corp','2012 Corp','2013 Corp','2014 Corp','2015 Corp','2000 Indv','2001 Indv','2002 Indv','2003 Indv','2004 Indv','2005 Indv','2006 Indv','2007 Indv','2008 Indv','2009 Indv','2010 Indv','2011 Indv','2012 Indv','2013 Indv','2014 Indv','2015 Indv'])
-    
+    header_summary = ['Indent', 'Budget Function', 'Subsidyscope Title', 'Title as Appears in Budget', 'Source', 'Footnotes', '2000 Total','2001 Total','2002 Total','2003 Total','2004 Total','2005 Total','2006 Total','2007 Total','2008 Total','2009 Total','2010 Total','2011 Total','2012 Total','2013 Total','2014 Total','2015 Total', '2016 Total', '2000 Corp','2001 Corp','2002 Corp','2003 Corp','2004 Corp','2005 Corp','2006 Corp','2007 Corp','2008 Corp','2009 Corp','2010 Corp','2011 Corp','2012 Corp','2013 Corp','2014 Corp','2015 Corp', '2016 Corp','2000 Indv','2001 Indv','2002 Indv','2003 Indv','2004 Indv','2005 Indv','2006 Indv','2007 Indv','2008 Indv','2009 Indv','2010 Indv','2011 Indv','2012 Indv','2013 Indv','2014 Indv','2015 Indv', '2016 Indv']
+
+
     if parent:
         
         top_level_group = parent
@@ -105,40 +105,41 @@ def te_csv(request, group_id=None):
             
         budget_function = top_level_group.name
             
-        recurse_category(parent, writer, '', budget_function)
+        if Group.objects.filter(parent=parent).count() == 0:
+            header_summary.insert( 5, 'Report')
+            writer.writerow(header_summary)
+            line_item_csv(parent, writer, budget_function)
+        else:
+            writer.writerow(header_summary)
+            recurse_category(parent, writer, '', budget_function)
     
     else:
         for parent in Group.objects.filter(parent=None):
             budget_function = parent.name
+            writer.writerow(header_summary)
             recurse_category(parent, writer, '', budget_function)
     
     return response
 
-
-def recurse_category(parent, writer, indent, budget_function):
-    
-
-    #writer.writerow([indent, budget_function, parent.name])
-        
-    for expenditure in parent.expenditure_set.order_by('source', 'analysis_year'):
+def line_item_csv(parent, writer, budget_function):
+    for expenditure in parent.expenditure_set.all():
         
         row = []
-        row.append(indent)
+        row.append('') #no indent
         row.append(budget_function)
         row.append(parent.name)
         row.append(expenditure.name)
         row.append(expenditure.get_source_display())
         row.append(expenditure.analysis_year)
         row.append(expenditure.notes)
-        
+    
         corp_estimates = {}
         indv_estimates = {}
         total_estimates = {}
         
-        
-        for estimate in expenditure.estimate_set.all():
-            
-            total_estimates[estimate.estimate_year] = None
+        for estimate in expenditure.estimate_set.all().order_by('estimate_year'):
+            if estimate.corporations_amount or estimate.individuals_amount:
+                total_estimates[estimate.estimate_year] = None
             
             if estimate.corporations_notes == Estimate.NOTE_POSITIVE:
                 total_estimates[estimate.estimate_year] = 0
@@ -147,14 +148,13 @@ def recurse_category(parent, writer, indent, budget_function):
                 total_estimates[estimate.estimate_year] = 0
                 corp_estimates[estimate.estimate_year] = '>-50'
             else:
-                corp_estimates[estimate.estimate_year] = estimate.corporations_amount
-                
                 if estimate.corporations_amount:
+                    corp_estimates[estimate.estimate_year] = estimate.corporations_amount
                     total_estimates[estimate.estimate_year] = estimate.corporations_amount
             
             if estimate.individuals_notes == Estimate.NOTE_POSITIVE:
                 
-                if not total_estimates[estimate.estimate_year]:
+                if total_estimates.get(estimate.estimate_year) and not total_estimates[estimate.estimate_year]:
                     total_estimates[estimate.estimate_year] = 0
                     
                 indv_estimates[estimate.estimate_year] = '<50'
@@ -167,9 +167,9 @@ def recurse_category(parent, writer, indent, budget_function):
                 indv_estimates[estimate.estimate_year] = '>-50'
                 
             else:
-                indv_estimates[estimate.estimate_year] = estimate.individuals_amount
-                
                 if estimate.individuals_amount:
+                    indv_estimates[estimate.estimate_year] = estimate.individuals_amount
+                    
                     if total_estimates[estimate.estimate_year]:
                         total_estimates[estimate.estimate_year] += estimate.individuals_amount
                     else:
@@ -195,11 +195,98 @@ def recurse_category(parent, writer, indent, budget_function):
                 row.append('')    
         
         writer.writerow(row)                  
-            
-    indent += '*'
-    
-    for subgroup in Group.objects.filter(parent=parent):
+
+
+def recurse_category(parent, writer, indent, budget_function):
+
+    if not parent.parent:
+        writer.writerow([indent, budget_function, parent.name])
+    else:
+        for source in (1, 2):
+            row = []
+            row.append(indent)
+            row.append(budget_function)
+            row.append(parent.name)
+#    row.append(expenditure.name)
+#   row.append(parent.get_source_display())
+#    row.append(parent.analysis_year)
+           
+            first = True 
+            corp_estimates = {}
+            indv_estimates = {}
+            total_estimates = {}
         
-        recurse_category(subgroup, writer, indent, budget_function)
+            for expenditure in parent.expenditure_set.filter(source=source).order_by('analysis_year'):
+                
+                if first:
+                    row.append(expenditure.name)
+                    row.append(SOURCES[source])
+                    row.append(parent.notes)
+                    first = False
+
+                for estimate in expenditure.estimate_set.all().order_by('estimate_year'):
+                    if estimate.corporations_amount or estimate.individuals_amount:
+                        total_estimates[estimate.estimate_year] = None
                     
+                    if estimate.corporations_notes == Estimate.NOTE_POSITIVE:
+                        total_estimates[estimate.estimate_year] = 0
+                        corp_estimates[estimate.estimate_year] = '<50'
+                    elif estimate.corporations_notes == Estimate.NOTE_NEGATIVE:
+                        total_estimates[estimate.estimate_year] = 0
+                        corp_estimates[estimate.estimate_year] = '>-50'
+                    else:
+                        if estimate.corporations_amount:
+                            corp_estimates[estimate.estimate_year] = estimate.corporations_amount
+                            total_estimates[estimate.estimate_year] = estimate.corporations_amount
+                    
+                    if estimate.individuals_notes == Estimate.NOTE_POSITIVE:
+                        
+                        if total_estimates.get(estimate.estimate_year) and not total_estimates[estimate.estimate_year]:
+                            total_estimates[estimate.estimate_year] = 0
+                            
+                        indv_estimates[estimate.estimate_year] = '<50'
+                        
+                    elif estimate.individuals_notes == Estimate.NOTE_NEGATIVE:
+                        
+                        if not total_estimates[estimate.estimate_year]:
+                            total_estimates[estimate.estimate_year] = 0
+                        
+                        indv_estimates[estimate.estimate_year] = '>-50'
+                        
+                    else:
+                        if estimate.individuals_amount:
+                            indv_estimates[estimate.estimate_year] = estimate.individuals_amount
+                            
+                            if total_estimates[estimate.estimate_year]:
+                                total_estimates[estimate.estimate_year] += estimate.individuals_amount
+                            else:
+                                total_estimates[estimate.estimate_year] = estimate.individuals_amount
+                
+                
+            for year in TE_YEARS:
+                if total_estimates.has_key(year):
+                    row.append(total_estimates[year])
+                else:
+                    row.append('')
+                
+            for year in TE_YEARS:
+                if corp_estimates.has_key(year):
+                    row.append(corp_estimates[year])
+                else:
+                    row.append('')
+                    
+            for year in TE_YEARS:                
+                if indv_estimates.has_key(year):
+                    row.append(indv_estimates[year])
+                else:
+                    row.append('')    
+            
+            writer.writerow(row)                  
+                
+    indent += '*'
+        
+    for subgroup in Group.objects.filter(parent=parent):
+            
+        recurse_category(subgroup, writer, indent, budget_function)
+                        
 
