57f4957756c90db1bebb9bccfb7d2e193dec6197
kaitlin <kaitlin@kaitlin-work.(none)>
2011-04-07 12:27:27 -0400

    lots of contract sort changes

--- trunk/contractsort/urls.py
+++ trunk/contractsort/urls.py
@@ -4,6 +4,7 @@ urlpatterns = patterns('',
     url(r'^$', 'contractsort.views.main', name='contractsort-main'),
     url(r'^sector/(?P<sector_id>[0-9]+)/$', 'contractsort.views.sector', name='contractsort-sector'),
     url(r'^sector/(?P<sector_id>[0-9]+)/updateagencies', 'contractsort.views.update_agency', name='contractsort-update-agency'),
+    url(r'setcodes/(?P<sector>[0-9]+)', 'contractsort.views.setcodes'),
     url(r'^login/$', 'contractsort.views.login', name='contractsort-login'),
     url(r'^logout/$', 'contractsort.views.logout', name='contractsort-logout'),
     )
--- trunk/contractsort/views.py
+++ trunk/contractsort/views.py
@@ -8,12 +8,45 @@ from fpds.models import NAICSCode, ProductOrServiceCode, FPDSRecord
 from django.http import HttpResponse
 from django.template import Template, Context
 from django.db import connection
+from django.db.models import Sum
+import re
 
 #@login_required
 def main(request):
     sectors = Sector.objects.all()
     return render_to_response('contractsort/main.html', {'sectors': sectors})
 
+def setcodes(request, sector):
+    sector = Sector.objects.get(id=sector)
+    selected = []
+    if request.GET:
+        if len(request.GET.lists()[0][1]) > 0:
+            current_naics = NAICSCode.objects.filter(sectors=sector)
+            current_psc = ProductOrServiceCode.objects.filter(sectors=sector)
+            for cn in current_naics:
+                cn.sectors.clear()
+            for cp in current_psc:
+                cp.sectors.clear()
+
+        for a in request.GET.lists()[0][1]:
+            codes = a.split('_')
+            if codes[0] == 'naics':
+                nc = NAICSCode.objects.get(code=int(codes[1]))
+                # add to sector list but make sure it's not already there
+                nc.sectors.add(sector)
+                selected.append(nc)
+            elif codes[0] == 'psc':
+                pc = ProductOrServiceCode.objects.get(code=codes[1])
+                pc.sectors.add(sector)
+                selected.append(pc)
+        
+        selected_naics_records = FPDSRecord.objects.filter(principal_naicscode__in=selected, fiscal_year=2009)
+        selected_psc_records = FPDSRecord.objects.filter(principal_naicscode__isnull=True, product_or_service_code__in=selected, fiscal_year=2009)
+        total_selected = (selected_naics_records.aggregate(Sum('obligated_amount'))['obligated_amount__sum'] or 0) + (selected_psc_records.aggregate(Sum('obligated_amount'))['obligated_amount__sum'] or 0)
+
+        total =  re.sub(r'(\d{3})(?=\d)', r'\1,', str(total_selected)[::-1])[::-1].split('.')[0]
+
+        return HttpResponse(Template('{{total}}').render(Context({'total': total })))
 
 #@login_required
 def sector(request, sector_id):
@@ -24,17 +57,17 @@ def sector(request, sector_id):
     selected = sector.related_agencies.all()
     agency_codes = []
     agency_id = []
+    selected_naics_records = FPDSRecord.objects.filter(principal_naicscode__in=sector_naics, fiscal_year=2009)
+    selected_psc_records = FPDSRecord.objects.filter(principal_naicscode__isnull=True, product_or_service_code__in=sector_psc, fiscal_year=2009)
+    total_selected = (selected_naics_records.aggregate(Sum('obligated_amount'))['obligated_amount__sum'] or 0) + (selected_psc_records.aggregate(Sum('obligated_amount'))['obligated_amount__sum'] or 0)
     for s in selected:
         agency_codes.append(s.fips_code)
         agency_id.append(str(s.id))
-    import logging
     if agency_id:
         q = "SELECT principal_naicscode_id, SUM(obligated_amount) as total from fpds_fpdsrecord where maj_agency_cat IN (%s) group by principal_naicscode_id" % ",".join(agency_codes)
-        logging.debug(q)
         curs = connection.cursor()
         curs.execute(q)
         naics = curs.fetchall()
-        logging.debug(naics)
         naics_list = []
         for n in naics:
             try:
@@ -45,14 +78,11 @@ def sector(request, sector_id):
                 parent_desc = "%s - %s" % (obj.parent_code.code, obj.parent_code.name)
                 naics_list.append((code, name, total, parent_desc))
             except Exception as e:
-                logging.debug(e)
                 pass
         
         q = "SELECT product_or_service_code_id, SUM(obligated_amount) as total from fpds_fpdsrecord where maj_agency_cat IN (%s) GROUP BY product_or_service_code_id" % ",".join(agency_codes)
-        logging.debug(q)
         curs.execute(q)
         psc = curs.fetchall()
-        logging.debug(psc)
         psc_list = []
         for p in psc:
             try:
@@ -62,16 +92,20 @@ def sector(request, sector_id):
                 total = p[1]
                 psc_list.append((code, name, total))
             except Exception as e:
-                logging.debug(e)
+                pass
     else:
         naics_list = None
         psc_list = None
+
+    size = sector_naics.count() + sector_psc.count()
+    if size < 10:
+        size = 30
 #    naicscode_ids = FPDSRecord.objects.filter(agency_id__in=selected).values_list('principal_naicscode').distinct()
  #   logging.debug(naicscode_ids)
   #  naicscodes = NAICSCode.objects.filter(code__in=naicscode_ids)
 
 
-    return render_to_response('contractsort/sector.html', {'sector': sector, 'agencies': agencies, 'selected': selected, 'naics': naics_list, 'psc': psc_list, 'sector_naics': sector_naics, 'sector_psc': sector_psc, 'size': sector_naics.count() + sector_psc.count()})
+    return render_to_response('contractsort/sector.html', {'sector': sector, 'agencies': agencies, 'selected': selected, 'naics': naics_list, 'psc': psc_list, 'sector_naics': sector_naics, 'sector_psc': sector_psc, 'size': size, 'total': total_selected})
 
 
 #@login_required
