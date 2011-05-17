5c65d64046414fc7e346e8d9f1cb36f8f9e3354e
caitlin <caitlin@Caitlin-Webers-iMac.local>
2011-05-17 17:27:59 -0400

    Nav and homepage restructuring.

---
+++ trunk/local_settings.py
@@ -0,0 +1,19 @@
+from settings import INSTALLED_APPS, MIDDLEWARE_CLASSES 
+
+DATABASE_ENGINE='mysql'
+DATABASE_NAME = 'subsidyscope'
+DATABASE_USER = 'root'
+DATABASE_PASSWORD = ''
+DATABASE_HOST = ''
+DATABASE_PORT = ''
+
+DEBUG=True
+
+STATIC_MEDIA_DIR = '/Users/caitlin/Projects/subsidyscope/trunk/media/'
+MEDIA_ROOT='/Users/caitlin/Projects/subsidyscope/trunk/media/'
+
+TEMPLATE_DIRS = (
+    '/Users/caitlin/Projects/subsidyscope/trunk/templates/',
+)
+
+MORSELS_JAVASCRIPT_PATH = ''
